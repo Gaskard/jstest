275506a6f674698d34e374a8a411488266137ce0
Dmytro <dimacr137@gmail.com>
2024-06-24 09:48:03 +0300

    test commit

--- js/script.js
+++ js/script.js
@@ -1,45 +1,14 @@
 "use strict";
 
-/* 4) Представьте такую реальную ситуацию. У вас есть банкомат, который выдает деньги из двух разных банков в разных валютах. Один банк основной с базовыми валютами, второй дополнительный с прочими валютами:
-
-const baseCurrencies = ['USD', 'EUR'];
-const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
-Вам нужно создать главную функцию банкомата availableCurr, которая принимает два аргумента: первый - это массив со всеми доступными валютами из двух банков сразу (сейчас представим, что они не могут повторяться), второй - необязательный аргумент, который указывает ту валюту, которая сейчас закончилась в банкомате. Если массив в первом аргументе пустой - то функция возвращает строку 'Нет доступных валют'. Функция возвращает строку в нужном виде.
-
-Пример:
-
-availableCurr(['UAH', 'RUB', 'CNY'], 'CNY')
-Вернет строку:
-
-Доступные валюты:
-UAH
-RUB
-Заметьте:
-
-- CNY (юань) исчез из списка валют, значит такая валюта закончилась
-
-- После валюты: стоит перенос строки \n, и после каждой валюты тоже. Это важно для тестов
-
-- Данные для первого аргумента должны приходить сразу из двух банков, причем сначала baseCurrencies, потом additionalCurrencies по порядку */
-
-
-
-
-const baseCurrencies = ['USD', 'EUR'];
-const additionalCurrencies = ['UAH', 'RUB', 'CNY'];
-
-function availableCurr(arr, missingCurr) {
-    let str = '';
-    arr = baseCurrencies.concat(additionalCurrencies);
-    arr.length === 0 ? str = 'Нет доступных валют' : str = 'Доступные валюты:\n';
-
-    arr.forEach(function(item) {
-        if (item !== missingCurr) {
-            str += `${item}\n`
-        }
-    })
-    return str;
+let soldier = {
+    health: 100,
+    armor: 50
 }
 
+let john = {
+    health: 500
+}
 
-console.log(availableCurr(['UAH', 'RUB', 'CNY'], 'CNY'))
+john.__proto__ = soldier;
+
+console.log(john)
